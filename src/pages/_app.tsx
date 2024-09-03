bb9a9e2fa28ef671b108a1e982a7d7adc49e7d0c
Adrian Markowski <amarkowski@student.agh.edu.pl>
2024-09-03 21:05:04 +0200

    chore: add ESLint & Prettier to codebase (#1)
    * chore: add eslint&prettier
    
    * chore: improve gitignore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,33 +5,32 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../theme';
 
-export default function MyApp(props: { Component: any; pageProps: any; }) {
-    const { Component, pageProps } = props;
+export default function MyApp(props: { Component: any; pageProps: any }) {
+  const { Component, pageProps } = props;
 
-    React.useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-            // @ts-ignore
-            jssStyles.parentElement.removeChild(jssStyles);
-        }
-    }, []);
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
 
-    return (
-        <React.Fragment>
-            <Head>
-                <title>My Next.js App</title>
-    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        </Head>
-        <ThemeProvider theme={theme}>
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My Next.js App</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...pageProps} />
-    </ThemeProvider>
+      </ThemeProvider>
     </React.Fragment>
-);
+  );
 }
 
 MyApp.propTypes = {
-    Component: PropTypes.elementType.isRequired,
-    pageProps: PropTypes.object.isRequired,
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
 };
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,14 +1,14 @@
-import TopNavBar from "@/components/TopNavBar/TopNavBar";
-import React from "react";
+import TopNavBar from '@/components/TopNavBar/TopNavBar';
+import React from 'react';
 
-export default function Contact(){
-    return (
-        <>
-            <TopNavBar/>
-        <div>
-            <h1>Contact</h1>
-            <p>This is the contact page</p>
-        </div>
-            </>
-    )
+export default function Contact() {
+  return (
+    <>
+      <TopNavBar />
+      <div>
+        <h1>Contact</h1>
+        <p>This is the contact page</p>
+      </div>
+    </>
+  );
 }
